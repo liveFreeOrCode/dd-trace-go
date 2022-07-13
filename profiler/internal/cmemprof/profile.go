658f7daacbd91dd1872a29e831ff7233b364f54b
Kyle Nusbaum <kyle@datadog.com>
2022-07-13 14:07:06 -0500

    test

--- profiler/internal/cmemprof/profile.go
+++ profiler/internal/cmemprof/profile.go
@@ -3,8 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2022 Datadog, Inc.
 
-//go:build cgo !windows
-// +build cgo !window
+//go:build cgo && !windows
+// +build cgo,!windows
 
 // Package cmemprof profiles C memory allocations (malloc, calloc, realloc, etc.)
 //
